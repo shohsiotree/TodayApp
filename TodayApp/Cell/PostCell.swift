368d56f8cf6ce0587789ba877f82f4fbbbd87282
shoh <shoh8638@gmail.com>
2022-06-15 08:50:47 +0900

    FEAT: 초기 VC등 구성

---
+++ TodayApp/Cell/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  TodayApp
+//
+//  Created by shoh on 2022/06/15.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TodayApp/ViewController/Login&Join/LoginPageVC.swift
+++ TodayApp/ViewController/Login&Join/LoginPageVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginPageVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
