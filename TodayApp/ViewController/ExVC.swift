5a77a49d15b55ab4919e6e76b1185676addc3202
shoh <shoh8638@gmail.com>
2022-06-22 12:52:00 +0900

    FIX: main UI 적용 및 add / save / alarm button 적용

--- TodayApp/ViewController/ExVC.swift
+++ TodayApp/ViewController/ExVC.swift
@@ -11,12 +11,14 @@ class ExVC: UIViewController {
     
     @IBOutlet weak var toolbarStack: UIStackView!
     @IBOutlet weak var textToolbar: UIToolbar!
-    @IBOutlet weak var buttonToolbar: UIToolbar!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var stackHeight: NSLayoutConstraint!
     @IBOutlet weak var addButton: UIButton!
     @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var alramText: UILabel!
+    @IBOutlet weak var alarmText: UILabel!
+    @IBOutlet weak var timePicker: UIDatePicker!
+    @IBOutlet weak var timePickerHeight: NSLayoutConstraint!
+    @IBOutlet weak var alarmButton: UIButton!
     
     var keyHeight: CGFloat?
     var numberOfCount = 1
@@ -27,8 +29,11 @@ class ExVC: UIViewController {
         self.tableView.clipsToBounds = true
         self.tableView.dataSource = self
         self.tableView.delegate = self
-        self.alramText.isHidden = true
+        self.alarmText.text = ""
+        self.timePicker.backgroundColor = UIColor(named: "BackgroundColor")
+        self.timePicker.tintColor = .black
         self.view.bringSubviewToFront(self.addButton)
+        self.view.bringSubviewToFront(self.timePicker)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
@@ -37,26 +42,31 @@ class ExVC: UIViewController {
         self.view.endEditing(true)
         self.tableView.endEditing(true)
         if self.stackHeight.constant == 80.0 {
-            self.stackHeight.constant = 0.0
-            self.tableView.clipsToBounds = true
-            self.addButton.isHidden = false
-            self.textField.text = ""
-        }
-    }
-    
-    @IBAction func tapAddButton(_ sender: Any) {
-        if self.stackHeight.constant == 0.0 {
             UIView.animate(withDuration: 0.5) {
-                self.stackHeight.constant = 80.0
+                self.stackHeight.constant = 0.0
                 self.tableView.clipsToBounds = true
-                self.addButton.isHidden = true
-                self.textField.becomeFirstResponder()
+                self.addButton.isHidden = false
+                self.textField.text = ""
+                self.alarmText.text = ""
+                self.timePicker.alpha = 0
             }
         }
     }
     
+    @objc func tapAlaramButton() {
+        UIView.animate(withDuration: 0.5) {
+            self.timePicker.alpha = 1
+            self.alarmButton.setTitle("저장", for: .normal)
+        }
+    }
+    @objc func tapSavaButton() {
+        UIView.animate(withDuration: 0.5) {
+            self.timePicker.alpha = 0
+            self.alarmButton.setTitle("알람", for: .normal)
+        }
+    }
+    
     @objc func keyboardWillShow(_ sender: Notification) {
-        
         let userInfo:NSDictionary = sender.userInfo! as NSDictionary
         let keyboardFrame:NSValue = userInfo.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue
         let keyboardRectangle = keyboardFrame.cgRectValue
@@ -70,14 +80,63 @@ class ExVC: UIViewController {
         self.tableView.frame.size.height += self.keyHeight!
         self.view.frame.size.height += self.keyHeight!
     }
-    @IBAction func alramButton(_ sender: Any) {
-        //TODO: 알람 데이트 피커 열기
+    
+    @IBAction func tapAddButton(_ sender: Any) {
+        if self.stackHeight.constant == 0.0 {
+            UIView.animate(withDuration: 0.5) {
+                self.stackHeight.constant = 80.0
+                self.tableView.clipsToBounds = true
+                self.addButton.isHidden = true
+                self.textField.becomeFirstResponder()
+            }
+        }
     }
     
-    @IBAction func saveButton(_ sender: Any) {
+    @IBAction func tapAlarmButton(_ sender: Any) {
+        //TODO: 알람 데이트 피커 열기
+        if self.timePicker.alpha == 0 && self.alarmButton.currentTitle == "알람" {
+            UIView.animate(withDuration: 0.5) {
+                self.timePicker.alpha = 1
+                self.alarmButton.setTitle("추가", for: .normal)
+            }
+        } else if self.timePicker.alpha == 1 && self.alarmButton.currentTitle == "추가"{
+            UIView.animate(withDuration: 0.5) {
+                self.timePicker.alpha = 0
+                self.alarmButton.setTitle("알람", for: .normal)
+                self.alarmText.text = self.timeString(date: self.timePicker.date)
+                print(self.timeString(date: self.timePicker.date))
+            }
+        }
+    }
+    
+    func timeString(date: Date) -> String {
+        let formmater = DateFormatter()
+        formmater.timeStyle = .short
+        return formmater.string(from: date)
+    }
+    
+    func dateString() -> String {
+        let formmater = DateFormatter()
+        formmater.dateFormat = "YYYY년 MM월 dd일"
+        return formmater.string(from: Date())
+    }
+    
+    @IBAction func tapSaveButton(_ sender: Any) {
         if !self.textField.text!.isEmpty {
             guard let text = self.textField.text else { return }
-            print(text)
+            guard let alarm = self.alarmText.text else { return }
+            let nowDate = dateString()
+            
+            UIView.animate(withDuration: 0.5) {
+                self.stackHeight.constant = 0.0
+                self.tableView.clipsToBounds = true
+                self.addButton.isHidden = false
+                self.textField.text = ""
+                self.alarmText.text = ""
+                self.timePicker.alpha = 0
+                self.view.endEditing(true)
+            }
+            print("alarm: \(alarm), text: \(text), now: \(nowDate)")
         }
     }
 }
